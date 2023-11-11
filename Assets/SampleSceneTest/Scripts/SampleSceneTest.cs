13b28d55b308553c359581a48690f08b06e7fbc5
Andrew Chau <chau.andrew.s@gmail.com>
2023-11-10 23:55:19 -0800

    got basic reticle working and placing spore object, added text mesh pro

--- Assets/SampleSceneTest/Scripts/SampleSceneTest.cs
+++ Assets/SampleSceneTest/Scripts/SampleSceneTest.cs
@@ -1,18 +1,78 @@
-using System.Collections;
-using System.Collections.Generic;
+using Niantic.Lightship.AR.NavigationMesh;
 using UnityEngine;
 
 public class SampleSceneTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Camera arCamera = null;
+
+    [SerializeField]
+    private LightshipNavMeshManager navMeshManager = null;
+
+    [SerializeField]
+    private LightshipNavMeshRenderer navMeshRenderer = null;
+
+    [SerializeField]
+    private GameObject reticlePrefab = null;
+
+    [SerializeField]
+    private float reticleDistance = 2f;
+
+    [SerializeField]
+    private float reticleHeightAboveGround = .1f;
+
+    [SerializeField]
+    private GameObject objectToPlace = null;
+
+    private GameObject reticleInstance;
+
+    private float currentReticleHeight = 0;
+
+    void Awake()
     {
-        
+        reticleInstance = Instantiate(reticlePrefab);
+        reticleInstance.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (navMeshManager != null)
+        {
+            LightshipNavMesh navMesh = navMeshManager.LightshipNavMesh;
+
+            Ray ray = new Ray(arCamera.transform.position, arCamera.transform.forward);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, reticleDistance) && navMesh.IsOnNavMesh(hit.point, 0.2f))
+            {
+                if (reticleInstance != null && reticleInstance.activeSelf == false)
+                {
+                    reticleInstance.SetActive(true);
+                }
+
+                currentReticleHeight = hit.point.y;
+            }
+        }
+
+        UpdateReticlePosition();
+
+        if (Input.GetMouseButtonDown(0) && reticleInstance != null && reticleInstance.activeSelf)
+        {
+            Instantiate(objectToPlace, reticleInstance.transform.position, Quaternion.identity);
+        }
+    }
+
+    void UpdateReticlePosition()
+    {
+        if (reticleInstance != null)
+        {
+            Vector3 newPosition = arCamera.transform.position + arCamera.transform.forward * reticleDistance;
+            newPosition.y = currentReticleHeight + reticleHeightAboveGround;
+            reticleInstance.transform.position = newPosition;
+        }
     }
 }
+
+
+
+
